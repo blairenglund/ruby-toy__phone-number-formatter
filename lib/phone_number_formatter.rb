91afba6a194a9e9a1287018a4e20ea5e81f05293
Blair Englund <blair.englund@gmail.com>
2016-06-25 12:46:49 -0500

    separating string into substrings based on position in the array

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,5 +2,10 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-  return phone_number_str
+  area_code = [phone_number_str.slice(0..2)]
+  sub_prefix = [phone_number_str.slice(3..5)]
+  sub_number = [phone_number_str.slice(6..9)]
+  return area_code + sub_prefix + sub_number
 end
+
+format_phone_number("3089912943")
