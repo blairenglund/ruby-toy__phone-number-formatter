5cb86a235de12ef205e5cdbfc833a21f72d86fdc
Blair Englund <blair.englund@gmail.com>
2016-06-27 12:39:22 -0500

    should remove all spaces and dashes

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,10 +2,13 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-  area_code = phone_number_str.slice(0..2)
-  sub_prefix = phone_number_str.slice(3..5)
-  sub_number = phone_number_str.slice(6..9)
-  return "(#{area_code}) #{sub_prefix}-#{sub_number}"
+	phone_number_str.gsub!(/ /, "") #gets rid of errant spaces
+	phone_number_str.gsub!(/-/, "") #gets rid of errant dashes
+	area_code = phone_number_str.slice(0..2)
+	sub_prefix = phone_number_str.slice(3..5)
+	sub_number = phone_number_str.slice(6..9)
+	puts "(#{area_code}) #{sub_prefix}-#{sub_number}"
+	return "(#{area_code}) #{sub_prefix}-#{sub_number}"
 end
 
-format_phone_number("3089912943")
+format_phone_number("308 991-2943")
