c83f5fed2b257b8529d385fb063c74be5a334cee
Blair Englund <blair.englund@gmail.com>
2016-06-27 22:04:00 -0500

    passing all tests

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,13 +2,11 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-	phone_number_str.gsub!(/ /, "") #gets rid of errant spaces
-	phone_number_str.gsub!(/-/, "") #gets rid of errant dashes
+	phone_number_str.gsub!(" ", "") #gets rid of errant spaces
+	phone_number_str.gsub!("-", "") #gets rid of errant dashes
+	phone_number_str.gsub!(".", "") #gets rid of errant periods
 	area_code = phone_number_str.slice(0..2)
 	sub_prefix = phone_number_str.slice(3..5)
 	sub_number = phone_number_str.slice(6..9)
-	puts "(#{area_code}) #{sub_prefix}-#{sub_number}"
 	return "(#{area_code}) #{sub_prefix}-#{sub_number}"
 end
-
-format_phone_number("308 991-2943")
